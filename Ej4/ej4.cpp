91710b57b4a095b61685267ab786ab575f8c6f27
Santiago-Puentes <spuentes@udesa.edu.ar>
2025-03-21 14:18:05 -0300

    punto 4)b) completado

--- Ej4/ej4.cpp
+++ Ej4/ej4.cpp
@@ -18,21 +18,22 @@ bool sonIguales(const string& str1, const string& str2, size_t index = 0) {
 }
 
 int main() {
-    auto startTime = std::chrono::high_resolution_clock::now();
-    miProcesoAMedir();
-    auto endTime = std::chrono::high_resolution_clock::now();
-    auto elapsedTime = std::chrono::duration_cast<std::chrono::nanoseconds>(
-    endTime - startTime);
-    std::cout << "A miProcesoAMedir le tomó: " << elapsedTime.count() << "nanosegundos" << std::endl;
     string texto1 = "Este es un ejemplo de texto que tiene más de sesenta y cuatro caracteres.";
     string texto2 = "Este es un ejemplo de texto que tiene más de sesenta y cuatro caracteres.";
     string texto3 = "Este es un ejemplo diferente de texto con más de sesenta y cuatro caracteres.";
-
+    
+    auto startTime = chrono::high_resolution_clock::now();
+    
     bool resultado1 = sonIguales(texto1, texto2);
     bool resultado2 = sonIguales(texto1, texto3);
+    
+    auto endTime = chrono::high_resolution_clock::now();
+    auto elapsedTime = chrono::duration_cast<chrono::nanoseconds>(endTime - startTime);
 
     cout << "¿Texto1 y Texto2 son iguales? " << (resultado1 ? "Sí" : "No") << endl;
     cout << "¿Texto1 y Texto3 son iguales? " << (resultado2 ? "Sí" : "No") << endl;
+
+    cout << "A miProcesoSonIguales le tomó: " << elapsedTime.count() << " nanosegundos" << std::endl;
 
     return 0;
 }
